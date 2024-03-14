ad7a9faa04675ea4b149de2f26931c4a5f9a7938
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2024-03-14 12:13:42 +0100

    Corriger bash xdebugProfile: lancement du test phpunit depuis bash ok

--- tests/xdebugProfile.sh
+++ tests/xdebugProfile.sh
@@ -2,9 +2,9 @@
 
 phpfile="$1"
 idekey="idestart"
-mode="none"
+mode="debug"
 testid="1"
 
 if [ $mode == "profile" ] || [ $mode == "debug" ]; then
-    XDEBUG_SESSION_START=$idekey;FBCompareTestId=$testid XDEBUG_TRIGGER=$mode php -d'xdebug.remote_enable=1' -d'xdebug.remote_enable=1' -d'xdebug.remote_autostart=1' -d'xdebug.idekey='"$idekey" -f "$phpfile" -- "$@"
+    XDEBUG_SESSION_START=$idekey FBCompareTestId=$testid XDEBUG_TRIGGER=$mode php -d'xdebug.remote_enable=1' -d'xdebug.remote_enable=1' -d'xdebug.remote_autostart=1' -d"xdebug.idekey=$idekey" -f "$phpfile" "$2"
 fi
