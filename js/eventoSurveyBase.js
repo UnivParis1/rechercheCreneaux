08d417a594cbda37d474fabc81cc01d739b3e5bd
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2024-06-28 16:49:41 +0200

    Travail en cours, bug propositions

--- js/eventoSurveyBase.js
+++ js/eventoSurveyBase.js
@@ -63,6 +63,82 @@ var eventoDraftBase = {
 	"is_draft": 1
 };
 
+
+var eventoSurveyDraftPropositions = {
+	"id": null,
+	"title": null,
+	"path": null,
+	"place": "",
+	"description": null,
+	"created": {
+		"raw": 1718977569,
+		"formatted": "21/06/2024"
+	},
+	"updated": {
+		"raw": null,
+		"formatted": "21/06/2024"
+	},
+	"closed": null,
+	"settings": {
+		"limit_participants": 0,
+		"limit_participants_nb": "10",
+		"disable_answer_edition": 0,
+		"dont_notify_on_reply": 0,
+		"hide_answers": 0,
+		"hide_comments": 0,
+		"time_zone": "Europe/Paris",
+		"auto_close": 1721520000,
+		"reply_access": "opened_to_everyone",
+		"enable_anonymous_answer": 0,
+		"dont_receive_invitation_copy": 0,
+		"auto_close_max": 1750464000,
+		"auto_close_min": 1719014400
+	},
+	"questions": [
+		{
+			"title": "",
+			"type": "date",
+			"proposition_type": "default",
+			"position": 0,
+			"options": {
+				"force_unique_choice": 0,
+				"enable_maybe_choices": 0
+			},
+			"propositions": [
+				{
+					"base_day": 1718928000,
+					"local_base_day": 1718928000,
+					"type": "range_of_hours",
+					"base_time": 50400,
+					"end_time": 54000,
+					"label": "21/06/2024 - 16:00 à 17:00"
+				},
+				{
+					"base_day": 1719273600,
+					"local_base_day": 1719273600,
+					"type": "range_of_hours",
+					"base_time": 50400,
+					"end_time": 54000,
+					"label": "25/06/2024 - 16:00 à 17:00"
+				}
+			],
+			"constraints": [],
+			"id": "1652385",
+		}
+	],
+	"is_draft": 1,
+	"is_closed": false,
+	"owners": [
+		{
+			"name": "Etienne Bohm",
+			"email": "Etienne.Bohm@univ-paris1.fr"
+		}
+	],
+	"guests": [],
+	"nb_participants": 0,
+	"new_guests": []
+}
+
 var eventoSurveyBase = {
     "id": "",
     "title": "arenseigner",
