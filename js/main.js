4e456198c91cfe40ffb8d5bccb9a07f46e5d9767
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2024-11-13 17:09:58 +0100

    Changer requirejs: plugin requirejs-order deprecated remplacé par requirejs-step, chargement des js en étape (step) uniquement pour bootstrap

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 requirejs.config({
     baseUrl: "js/",
     'paths': {
-	// from v1.0.5 requirejs : //requirejs.org/docs/release/1.0.5/minified/order
-	'order': 'lib/order',
-	'jquery' :'../node_modules/jquery/dist/jquery.min',
+        'jquery' :'../node_modules/jquery/dist/jquery.min',
         'bootstrap' : '../node_modules/bootstrap/dist/js/bootstrap.bundle.min',
         'nouislider' : '../node_modules/nouislider/dist/nouislider.min',
         'moment' : '../node_modules/moment/min/moment-with-locales.min',
+        'step' : 'lib/step/step'
     },
     'shims' : {
         'jquery': {
@@ -26,10 +25,18 @@ requirejs.config({
             deps: ['nouislider']
         }
     },
+    config: {
+        step: {
+            steps: [
+                ['jquery'],
+                ['bootstrap']
+            ]
+        }
+    },
 // out: tentative d'optimisation de requirejs
     out: 'rjsopt.js'
 }
 );
 
-requirejs(['order!form', 'order!evento']);
+requirejs(['form', 'evento']);
 
