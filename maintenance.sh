1389ce172d07b772ed87444aa759fa0f23d298e3
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2024-12-18 11:41:38 +0100

    Ajouter: script maintenance.sh: lance une série de commande de gestion du projet: composer, clean des répertoires, sync de vendor et node_module sur test/prod; Supprimer: node_modules/ du projet pour alléger les sources du projet

--- maintenance.sh
+++ maintenance.sh
@@ -8,11 +8,38 @@ case $1 in
 	;;
 
 	"clearminjs")
-		echo rm js/`cat .env | grep RJSFILE | cut -d= -f2 | sed s/\.js/\.\*/`;
-		rm js/`cat .env | grep RJSFILE | cut -d= -f2 | sed s/\.js/\.\*/`;
+		# shellcheck disable=SC2046
+		echo rm js/$(grep RJSFILE .env | cut -d= -f2 | sed s/\.js/\.\*/);
+		rm js/"$(grep RJSFILE .env | cut -d= -f2 | sed s/\.js/\.\*/)";
 	;;
+	
+	"composerup")
+		echo "rm composer.lock && rm -Ri vendor/ && composer update --ignore-platform-reqs";
+		rm composer.lock;
+		rm -Rif vendor/;
+		composer update --ignore-platform-reqs;
+	;;
+	"yarnup")
+		rm yarn.lock;
+		rm -Rif .yarn;
+		rm -Rif node_modules/;
+	;;
+	"syncvendor")
+		test "$2" = "php-test" -o "$2" = "dagon" || { echo "spécifier la destination: php-test|dagon (test ou prod)"; exit 2; } ;
 
+		hostsync=$2;
+
+		rsync -av -e 'ssh -K ' vendor/ creneaux@"$hostsync":~/www/vendor/ ;
+	;;
+	"syncnode")
+		test "$2" = "php-test" -o "$2" = "dagon" || { echo "spécifier la destination: php-test|dagon (test ou prod)"; exit 2; } ;
+
+		hostsync=$2;
+
+		rsync -av -e 'ssh -K ' node_modules/ creneaux@"$hostsync":~/www/node_modules ;
+
+	;;
 	*)
-		echo "optijs|clearminjs"
+		echo "optijs|clearminjs|composerup|yarnup|syncvendor|syncnode" 1>&2 && exit 2;
 	;;
 esac
