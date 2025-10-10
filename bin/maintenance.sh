b4609b598b7345079611add0b9042661872bf873
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2025-10-10 12:21:03 +0200

    Ajouter: sur bin/maintenance.sh : variables user et hostdir pour éviter d'avoir des données spécifique à UP1

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -2,6 +2,14 @@
 
 yarnbin="yarnpkg"
 
+test ! -f .env && echo "pas de fichier .env" && exit 1;
+
+user=$(grep CAS_URI_APP .env | cut -d'=' -f2)
+hostdir=$(grep HOSTDIR .env | cut -d'=' -f2)
+
+test ! -n "$user" && echo "pas de variable CAS_URI_APP dans .env" && exit 1;
+test ! -n "$hostdir" && echo "pas de variable HOSTDIR dans .env" && exit 1;
+
 test ! -d bin/ && test ! -d src/ && test ! -d vendor/ && echo "pas de repertoire bin/ ou src/ ou vendor/ ce script doit être appelé à la racine du projet" && exit 1;
 
 case $1 in
@@ -25,6 +33,7 @@ case $1 in
 				rm -Rif node_modules/;
 				touch yarn.lock;
 				$yarnbin install;
+				$yarnbin run build
 			;;
 		esac
 	;;
@@ -39,12 +48,13 @@ case $1 in
 				bin/maintenance.sh sync vendor $3 && bin/maintenance.sh sync node $3
 			;;
 			"node")
-				echo rsync -av -e 'ssh -K ' node_modules/ creneau@"$hostsync":~/www/node_modules;
-				rsync -av -e 'ssh -K ' node_modules/ creneau@"$hostsync":~/www/node_modules;
+				echo rsync -av -e 'ssh -K ' node_modules/ "$user"@"$hostsync":"$hostdir"/node_modules;
+				rsync -av -e 'ssh -K ' node_modules/ "$user"@"$hostsync":"$hostdir"/node_modules;
+				rsync -av -e 'ssh -K ' public/build/ "$user"@"$hostsync":"$hostdir"/public/build/;
 			;;
 			"vendor")
-				echo rsync -av -e 'ssh -K ' vendor/ creneau@"$hostsync":~/www/vendor/;
-				rsync -av -e 'ssh -K ' vendor/ creneau@"$hostsync":~/www/vendor/;
+				echo rsync -av -e 'ssh -K ' vendor/ "$user"@"$hostsync":"$hostdir"/vendor/;
+				rsync -av -e 'ssh -K ' vendor/ "$user"@"$hostsync":"$hostdir"/vendor/;
 			;;
 		esac
 	;;
@@ -79,7 +89,24 @@ case $1 in
 		esac;
 	;;
 
+	"optijs")
+		test -z "$2" && echo "optijs build / clear" && exit 2;
+
+		case $2 in
+			"build")
+				./maintenance.sh optijs clear
+				echo bin/optimize.sh js/build.js terser;
+				bin/optimize.sh js/build.js terser;
+			;;
+			"clear")
+				echo rm js/$(grep RJSFILE .env | cut -d= -f2 | sed s/\.js/\.\*/);
+				rm js/$(grep RJSFILE .env | cut -d= -f2 | sed s/\.js/\.\*/);
+			;;
+		esac;
+	;;
+
+
 	*)
-		echo "build / clear" 1>&2 && exit 1;
+		echo "build / sync/ clear" 1>&2 && exit 1;
 	;;
 esac
