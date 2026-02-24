aa80006b9eb1d0f9cfb7aebe3bc0aab7c4c81a84
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2026-02-24 11:14:00 +0100

    Centrer: mineur: visuel modal bootstrap, ajout de la classe modal-dialog-centered, présente pour la modale evento mais absente de celle pour les invitations

--- src/includes/modal.inc.php
+++ src/includes/modal.inc.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="creneauMailInput" tabindex="-1" aria-labelledby="modalInputLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <p>Envoi invitation aux participants</p>
