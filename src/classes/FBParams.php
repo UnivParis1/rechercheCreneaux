bf648d3fbddd7b9acb775fa476546827e336a408
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2026-05-18 14:46:45 +0200

    Réorgoniser: affectation des variables env mis dans la méthode static FBParams::init

--- src/classes/FBParams.php
+++ src/classes/FBParams.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace RechercheCreneaux;
 
 use DateTime;
-use Exception;
+use Dotenv\Dotenv;
+use phpCAS;
 use stdClass;
 
 /**
@@ -115,5 +116,106 @@ class FBParams
                 $this->uids[] = $aUid;
             }
         }
+    }
+
+    public static function initEnv(Dotenv $dotenv): stdClass
+    {
+        global $_GET;
+
+        $dotenv->load();
+
+        // valeures requises dans le fichier .env exception levée si ce n'est pas le cas
+        $dotenv->required(['ENV', 'APP_URL', 'URL_FREEBUSY', 'TIMEZONE', 'LOCALE']);
+        $dotenv->required('RECHERCHE_SUR_X_JOURS')->isInteger();
+
+        setlocale(LC_TIME, $_ENV['LOCALE']);
+
+        $stdEnv = new stdClass();
+        $stdEnv->env = (isset($_ENV['ENV'])) ? $_ENV['ENV'] : 'dev';
+
+        $stdEnv->url = $_ENV['URL_FREEBUSY'];
+        $stdEnv->dtz = $_ENV['TIMEZONE'];
+        $stdEnv->rechercheSurXJours = intval($_ENV['RECHERCHE_SUR_X_JOURS']);
+
+        $dotenv->required(['WSGROUP', 'PHOTO_SHOW', 'PROLONGATION_BANDEAU', 'CAS', 'ZOOM', 'EVENTO', 'AGENDASDISTANTS'])->isBoolean();
+
+        $stdEnv->appUrl = $_ENV['APP_URL'];
+
+        isset($_ENV['WSGROUP']) ? $stdEnv->wsgroup = (bool) json_decode(strtolower($_ENV['WSGROUP'])) : $stdEnv->wsgroup=false;
+        isset($_ENV['PHOTO_SHOW']) ? $stdEnv->photoShow = (bool) json_decode(strtolower($_ENV['PHOTO_SHOW'])) : $stdEnv->photoShow=false;
+        isset($_ENV['PROLONGATION_BANDEAU']) ? $stdEnv->prolongationBandeau = (bool) json_decode(strtolower($_ENV['PROLONGATION_BANDEAU'])) : $stdEnv->prolongationBandeau=false;
+        isset($_ENV['CAS']) ? $stdEnv->cas = (bool) json_decode(strtolower($_ENV['CAS'])) : $stdEnv->cas=false;
+        isset($_ENV['ZOOM']) ? $stdEnv->zoom = (bool) json_decode(strtolower($_ENV['ZOOM'])) : $stdEnv->zoom=false;
+        isset($_ENV['EVENTO']) ? $stdEnv->evento = (bool) json_decode(strtolower($_ENV['EVENTO'])) : $stdEnv->evento=false;
+        isset($_ENV['KRONOLITH']) ? $stdEnv->kronolith = (bool) json_decode(strtolower($_ENV['KRONOLITH'])) : $stdEnv->kronolith=false;
+        isset($_ENV['AGENDASDISTANTS']) ? $stdEnv->agendasDistants = (bool) json_decode(strtolower($_ENV['AGENDASDISTANTS'])) : $stdEnv->agendasDistants=false;
+        isset($_ENV['AGENDASDISTANTSUPLOAD']) && $stdEnv->agendasDistants ? $stdEnv->agendasDistantsUpload = (bool) json_decode(strtolower($_ENV['AGENDASDISTANTSUPLOAD'])) : $stdEnv->agendasDistantsUpload = false;
+
+        if ($stdEnv->wsgroup === true) {
+            $dotenv->required(['URLWSGROUP_USERS_AND_GROUPS', 'URLWSGROUP_USER_INFOS']);
+            $stdEnv->urlwsgroupUsersAndGroups = $_ENV['URLWSGROUP_USERS_AND_GROUPS'];
+            $stdEnv->urlwsgroupUserInfos = $_ENV['URLWSGROUP_USER_INFOS'];
+        }
+
+        if ($stdEnv->photoShow === true) {
+            $dotenv->required('URLWSPHOTO');
+            $stdEnv->urlwsphoto = $_ENV['URLWSPHOTO'];
+        }
+
+        if ($stdEnv->prolongationBandeau === true) {
+            $dotenv->required(['PROLONGATION_ENT_JS', 'PROLONGATION_ENT_ARGS_CURRENT']);
+
+            $stdEnv->prolongationEntJs = $_ENV['PROLONGATION_ENT_JS'];
+            $stdEnv->prolongationEntArgsCurrent = $_ENV['PROLONGATION_ENT_ARGS_CURRENT'];
+        }
+
+        if ($stdEnv->cas === true) {
+            $dotenv->required(['CAS_HOST', 'CAS_PORT', 'CAS_PATH', 'APP_URL']);
+
+            phpCAS::client(CAS_VERSION_2_0, $_ENV['CAS_HOST'], intval($_ENV['CAS_PORT']), $_ENV['CAS_PATH'], $_ENV['APP_URL']);
+            phpCAS::setNoCasServerValidation();
+
+            phpCAS::forceAuthentication();
+
+            if (!phpCAS::isAuthenticated()) {
+                header('HTTP/1.1 401 Unauthorized');
+                echo "Recherche_de_creneaux CAS Error authentificated";
+                exit;
+            }
+            $stdEnv->uidCasUser = phpCAS::getUser();
+        }
+
+        if ($stdEnv->zoom === true) {
+            $dotenv->required(['ZOOM_ACCOUNT_ID', 'ZOOM_CLIENT_ID', 'ZOOM_CLIENT_SECRET', 'ZOOM_LIB_CREDENTIAL_PATH']);
+            $stdEnv->zoomAccountId = $_ENV['ZOOM_ACCOUNT_ID'];
+            $stdEnv->zoomClientId = $_ENV['ZOOM_CLIENT_ID'];
+            $stdEnv->zoomClientSecret = $_ENV['ZOOM_CLIENT_SECRET'];
+            $stdEnv->zoomLibCredentialPath = $_ENV['ZOOM_LIB_CREDENTIAL_PATH'];
+
+            if (!file_exists($stdEnv->zoomLibCredentialPath)) {
+                file_put_contents($stdEnv->zoomLibCredentialPath, '');
+            }
+        }
+
+        if ($stdEnv->evento == true) {
+            $dotenv->required(['EVENTO_WS_URL', 'EVENTO_SHIBENTITYID']);
+            $stdEnv->eventoWsUrl = $_ENV['EVENTO_WS_URL'];
+            $stdEnv->eventoShibentityid = $_ENV['EVENTO_SHIBENTITYID'];
+        }
+
+        if ($stdEnv->kronolith == true) {
+            $dotenv->required(['KRONOLITH_HOST', 'KRONOLITH_IMPORT_URL_USER']);
+            $stdEnv->kronolith_host = $_ENV['KRONOLITH_HOST'];
+            $stdEnv->kronolith_import_url_user = $_ENV['KRONOLITH_IMPORT_URL_USER'];
+        }
+
+
+        $stdEnv->mailfrom = $_ENV['MAILFROM'] ?? null;
+
+        date_default_timezone_set($stdEnv->dtz);
+
+        $stdEnv->varsHTTPGet = filter_var_array($_GET);
+
+        return $stdEnv;
     }
 }
