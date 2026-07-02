de22fc2b91a658f6461bd81c91e03ada660ff1ba
Etienne Bohm <Etienne.Bohm@univ-paris1.fr>
2026-07-02 15:41:57 +0200

    Corriger: paramètre AGENDADISTANTS en AGENDAS_DISTANTS pour norme camelcase

--- src/includes/head_inc.php
+++ src/includes/head_inc.php
@@ -28,7 +28,7 @@ $stdEnv->url = $_ENV['URL_FREEBUSY'];
 $stdEnv->dtz = $_ENV['TIMEZONE'];
 $stdEnv->rechercheSurXJours = intval($_ENV['RECHERCHE_SUR_X_JOURS']);
 
-$dotenv->required(['WSGROUP', 'PHOTO_SHOW', 'PROLONGATION_BANDEAU', 'CAS', 'ZOOM', 'EVENTO', 'AGENDASDISTANTS'])->isBoolean();
+$dotenv->required(['WSGROUP', 'PHOTO_SHOW', 'PROLONGATION_BANDEAU', 'CAS', 'ZOOM', 'EVENTO', 'AGENDAS_DISTANTS'])->isBoolean();
 
 $stdEnv->appUrl = $_ENV['APP_URL'];
 $stdEnv->wsgroup = (bool) json_decode(strtolower($_ENV['WSGROUP']));
@@ -38,7 +38,7 @@ $stdEnv->cas = (bool) json_decode(strtolower($_ENV['CAS']));
 $stdEnv->zoom = (bool) json_decode(strtolower($_ENV['ZOOM']));
 $stdEnv->evento = (bool) json_decode(strtolower($_ENV['EVENTO']));
 $stdEnv->kronolith = (bool) json_decode(strtolower($_ENV['KRONOLITH']));
-$stdEnv->agendasDistants = (bool) json_decode(strtolower($_ENV['AGENDASDISTANTS']));
+$stdEnv->agendasDistants = (bool) json_decode(strtolower($_ENV['AGENDAS_DISTANTS']));
 
 if ($stdEnv->wsgroup === true) {
     $dotenv->required(['URLWSGROUP_USERS_AND_GROUPS', 'URLWSGROUP_USER_INFOS']);
